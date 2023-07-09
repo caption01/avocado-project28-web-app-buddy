0b82b560ab3c3c9ad923f3f76cb8c4ae9c8f810c
nutchapon hanouypornlert <salaryhero@nutchapons-MacBook-Air.local>
2023-07-09 12:37:40 +0700

    second page

--- src/app/components/molecule/Title/index.tsx
+++ src/app/components/molecule/Title/index.tsx
@@ -5,11 +5,12 @@ import { Text } from '@/app/components';
 interface TitleProps {
   bold?: boolean;
   children: ReactNode;
+  className?: string;
 }
 
 const Title = (props: TitleProps) => {
   return (
-    <Text className='text-5xl' bold={props.bold}>
+    <Text className={`text-5xl ${props?.className}`} bold={props.bold}>
       {props.children}
     </Text>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Header, Title, Screen, Button } from '@/app/components';
 
 export default function Page() {
   return (
-    <div>
+    <div className='container mx-auto px-4'>
       <Screen className='h-screen'>
         <div className='grid grid-cols-1 grid-rows-1 h-full'>
           <div className='flex justify-center items-center'>
@@ -13,14 +13,31 @@ export default function Page() {
               <Header className='mb-4' bold>
                 Avocado Shop
               </Header>
-              <Button className='bg-gray-500'>Buy Now</Button>
+              <Button className='border-2 border-black'>Buy Now</Button>
             </div>
           </div>
         </div>
       </Screen>
       <Screen className='h-screen'>
-        <Title bold>Avocado everyday.</Title>
-        <Title bold>Keep doctor awy</Title>
+        <div className='grid grid-cols-1 grid-rows-1 h-full'>
+          <div>
+            <Header className='mb-4' bold>
+              Avocado everyday
+            </Header>
+            <Header className='mb-24' bold>
+              Keep doctor awy
+            </Header>
+            <Title className='leading-relaxed'>
+              Eating avocado every day has numerous health benefits. Avocados
+              are a{' '}
+              <span className='underline underline-offset-2 decoration-4 mr-4'>
+                great source of healthy fats, fiber, and vitamins.
+              </span>
+              Consuming them regularly can help improve heart health by lowering
+              cholesterol levels and reducing the risk of heart disease.
+            </Title>
+          </div>
+        </div>
       </Screen>
       <Screen className='h-screen'>
         <Header>Recipe</Header>
